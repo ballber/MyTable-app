cf02a046b2bd6f9cb5c3c287336401fca9230af2
Josep B <ballber@gmail.com>
2018-11-05 01:06:47 +0100

    Initial Commit

---
+++ MyTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTable
+//
+//  Created by Joe on 5/11/18.
+//  Copyright © 2018 Universitat Oberta de Catalunya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
