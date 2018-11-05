f9d4cfa6f255d48477c32614bd9838fb859306c5
Josep B <ballber@gmail.com>
2018-11-05 02:12:24 +0100

    Creat ElementCell

---
+++ ElementCell.swift
@@ -0,0 +1,24 @@
+//
+//  ElementCell.swift
+//  MyTable
+//
+//  Created by Joe on 5/11/18.
+//  Copyright © 2018 Universitat Oberta de Catalunya. All rights reserved.
+//
+
+import UIKit
+
+class ElementCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
